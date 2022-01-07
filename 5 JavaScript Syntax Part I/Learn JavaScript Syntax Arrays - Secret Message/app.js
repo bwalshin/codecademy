4713317120936d01d4611f5f3a35be673f071d8b
bwalshin <brandon.walshin@gmail.com>
2022-01-07 13:18:34 -0800

    Initial upload

---
+++ 5 JavaScript Syntax Part I/Learn JavaScript Syntax Arrays - Secret Message/app.js
@@ -0,0 +1,15 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+secretMessage.push('to', 'program');
+
+const easilyIndex = secretMessage.indexOf('easily');
+secretMessage[easilyIndex] = 'right';
+
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6,5, 'know');
+
+// console.log(secretMessage);
+
+console.log(secretMessage.join(' '));
