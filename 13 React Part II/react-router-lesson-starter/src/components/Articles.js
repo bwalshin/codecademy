eb297243aca438e04da64b562fb15c34b01a2d62
bwalshin <brandon.walshin@gmail.com>
2022-03-24 12:55:11 -0700

    Lesson updates

--- 13 React Part II/react-router-lesson-starter/src/components/Articles.js
+++ 13 React Part II/react-router-lesson-starter/src/components/Articles.js
@@ -4,16 +4,15 @@ import { selectArticles, filterArticles } from "../features/articles/articlesSli
 import Search from "./Search";
 
 // Import Link from React Router
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Articles () {
   const articles = useSelector(selectArticles)
   
   // grab the search value from useLocation()
-  
-  // get the queryParams from new URLSearchParams() 
-  
-  const title = '' // <-- fix me!
+  const { search } = useLocation();
+  const queryParams = new URLSearchParams(search);
+  const title = queryParams.get('title');
   
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
