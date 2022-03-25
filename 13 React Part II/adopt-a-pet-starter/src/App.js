5dadee8aaf9aead8fe665c79c35d519bd7f485e4
bwalshin <brandon.walshin@gmail.com>
2022-03-25 10:53:30 -0700

    Added Route to homepage

--- 13 React Part II/adopt-a-pet-starter/src/App.js
+++ 13 React Part II/adopt-a-pet-starter/src/App.js
@@ -3,15 +3,17 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-        <div>
-          <Navigation />
+      <div>
+        <Navigation />
+        <Route path="/:type?">
           <HomePage />
-        </div>
+        </Route>
+      </div>
     </Router>
   );
 }
