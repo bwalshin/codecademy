4b7bbc3a974e15531c8ebe84f365dd162e3b45d6
bwalshin <brandon.walshin@gmail.com>
2022-03-28 14:09:17 -0700

    - Defined contacts and appointemnt variables - Created set functions

--- 13 React Part II/react_challenge_project-appointment_planner/src/App.js
+++ 13 React Part II/react_challenge_project-appointment_planner/src/App.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
   };
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  const addAppointment = (title, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        title: title,
+        contact: contact,
+        date, date,
+        time: time,
+      },
+    ]);
+  };
+
+  const addContact = (name, phone, email) = > {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
+  };
 
   return (
     <>
