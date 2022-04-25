d3edf40c79789e70af91dae3d83acbdbc5226677
bwalshin <brandon.walshin@gmail.com>
2022-04-25 14:06:39 -0700

    - Applied defined styles to App.js - Imported keyframes and applied to App.js

--- Advance Web Development/getting-started-emotion-code/starter-code/src/App.js
+++ Advance Web Development/getting-started-emotion-code/starter-code/src/App.js
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
+import { css, ThemeProvider } from '@emotion/react';
 import logo from './logo.png';
-import { CardWrapper, ImageWrapper, TextWrapper, TitleWrapper, DescriptionWrapper, ActionsWrapper, PrimaryButton, SecondaryButton, } from './styles';
+import { LogoSpin, CardWrapper, ImageWrapper, TextWrapper, TitleWrapper, DescriptionWrapper, ActionsWrapper, PrimaryButton, SecondaryButton, theme} from './styles';
 // import `css`and `ThemeProvider` from "@emotion/react" package
 // import styled components, theming and animation from "./styles.js" file
 
@@ -32,54 +32,57 @@ const hotels = [
 // Apply styling to code within the `App` component's `return` statement using styled components, theming, animation and the `css` prop
 function App() {
   return (
-    <main
-      css={{
-        color: '#03045e',
-        background: '#caf0f8',
-        height: '1200px',
-        fontFamily: 'helvetica',
-      }}
-    >
-      <img
-        src={logo}
-        alt='logo'
-        css={css`
-          display: absolute;
-          margin-top: 15px;
-          margin-left: 15px;
-          height: 100px;
-          width: 100px;
-        `}
-      />
-      <div
-        css={css`
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          gap: 15px;
-          padding: 20px;
-          @media (max-width: 900px) {
-            display: grid;
-          }
-        `}
-      >
-        {hotels.map((hotel) => {
-          return (
-            <CardWrapper key={hotel.id}>
-              <ImageWrapper src={hotel.src} alt={hotel.alt} />
-              <TextWrapper>
-                <TitleWrapper>{hotel.title}</TitleWrapper>
-                <DescriptionWrapper>{hotel.description}</DescriptionWrapper>
-              </TextWrapper>
-              <ActionsWrapper>
-                <PrimaryButton>Details</PrimaryButton>
-                <SecondaryButton>Book</SecondaryButton>
-              </ActionsWrapper>
-            </CardWrapper>
-          );
+    <ThemeProvider theme={theme}>
+      <main
+        css={(theme) => ({
+          color: theme.colors.primary,
+          background: theme.colors.secondary,
+          height: '1200px',
+          fontFamily: theme.fonts.primary,
         })}
-      </div>
-    </main>
+      >
+        <img
+          src={logo}
+          alt='logo'
+          css={css`
+            display: absolute;
+            margin-top: 15px;
+            margin-left: 15px;
+            height: 100px;
+            width: 100px;
+            animation: ${LogoSpin} 10s linear infinite;
+          `}
+        />
+        <div
+          css={css`
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 15px;
+            padding: 20px;
+            @media (max-width: 900px) {
+              display: grid;
+            }
+          `}
+        >
+          {hotels.map((hotel) => {
+            return (
+              <CardWrapper key={hotel.id}>
+                <ImageWrapper src={hotel.src} alt={hotel.alt} />
+                <TextWrapper>
+                  <TitleWrapper>{hotel.title}</TitleWrapper>
+                  <DescriptionWrapper>{hotel.description}</DescriptionWrapper>
+                </TextWrapper>
+                <ActionsWrapper>
+                  <PrimaryButton>Details</PrimaryButton>
+                  <SecondaryButton>Book</SecondaryButton>
+                </ActionsWrapper>
+              </CardWrapper>
+            );
+          })}
+        </div>
+      </main>
+    </ThemeProvider>
   );
 }
 
