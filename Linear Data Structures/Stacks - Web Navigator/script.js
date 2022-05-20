d354f34525c4e86f1ea44c02fa5fe68bd7e69d18
bwalshin <brandon.walshin@gmail.com>
2022-05-20 16:23:39 -0700

    - Created files - Created Initialization variables

---
+++ Linear Data Structures/Stacks - Web Navigator/script.js
@@ -0,0 +1,29 @@
+const Stack = require('./Stack.js');
+const prompt = require('prompt-sync')();
+// ------------------------------
+// Initialization
+// ------------------------------
+const backPages = new Stack();
+const nextPages = new Stack();
+let currentPage = 'Start Page';
+
+// ------------------------------
+// Helper Functions
+// ------------------------------
+
+/*
+ * The following strings are used to prompt the user
+ */
+const baseInfo = '\nEnter a url';
+const backInfo = 'B|b for back page';
+const nextInfo = 'N|n for next page';
+const quitInfo = 'Q|q for quit';
+const question = 'Where would you like to go today? ';
+
+// ------------------------------
+// User Interface Part 1
+// ------------------------------
+
+// ------------------------------
+// User Interface Part 2
+// ------------------------------
